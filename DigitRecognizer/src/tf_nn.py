34b95bc6c5371ecee538bcdaa3e233042b37a718
Zhang Shuo <shuo.1.zhang@nokia.com>
2017-06-28 20:11:07 +0800

    complete training

--- DigitRecognizer/src/tf_nn.py
+++ DigitRecognizer/src/tf_nn.py
@@ -6,27 +6,44 @@ import tensorflow as tf
 
 
 def saveToJpeg(idx, images, labels):
+    """images is N*784 matrix and labels is N*10*1 matrix"""
     image = images[idx,:]
-    label = labels[idx]
+    labelVector = labels[idx]
+    for i in range(10):
+        if labelVector[i] != 0:
+            label = i
     imsave("sample"+str(idx)+"_num"+str(label)+".jpeg", image.reshape(28,28))
 
 
+def vectorized_result(idx):
+    """Return a 10-dimensional unit vector with a 1.0 in the jth
+    position and zeroes elsewhere.  This is used to convert a digit
+    (0...9) into a corresponding desired output from the neural
+    network."""
+    e = np.zeros((10, 1))
+    e[idx] = 1.0
+    return e
 
 
 
 
 TOTAL_TRAIN_SAMPLE_SIZE = 42000
-VALIDATION_SAMPLE_SIZE = 7000
+VALIDATION_SAMPLE_SIZE = 12000
 TRAIN_SAMPLE_SIZE = (TOTAL_TRAIN_SAMPLE_SIZE-VALIDATION_SAMPLE_SIZE)
-SAMPLE_SIZE_PER_BATCH = 100
+SAMPLE_SIZE_PER_BATCH = 200
 BATCH_NUMBER = (TRAIN_SAMPLE_SIZE/SAMPLE_SIZE_PER_BATCH)
 
 train = pd.read_csv("../input/train.csv")
 #test = pd.read_csv('../input/test.csv')
 trainImages = (train.ix[0:(TRAIN_SAMPLE_SIZE-1),1:].values).astype('float32')
-trainLabels = (train.ix[0:(TRAIN_SAMPLE_SIZE-1),0].values).astype('int32')
+trainOriginalLabels = (train.ix[0:(TRAIN_SAMPLE_SIZE-1),0].values).astype('int32')
+#numpy.ndarray to list to numpy.ndarray (35000, 10, 1) to numpy.ndarray (35000, 10) 
+trainLabels = (np.array([vectorized_result(label) for label in trainOriginalLabels])).reshape(TRAIN_SAMPLE_SIZE,10)
+
 validateImages = (train.ix[TRAIN_SAMPLE_SIZE:,1:].values).astype('float32')
-validateLabels = (train.ix[TRAIN_SAMPLE_SIZE:,0].values).astype('int32')
+validateOriginalLabels = (train.ix[TRAIN_SAMPLE_SIZE:,0].values).astype('int32')
+#numpy.ndarray to list to numpy.ndarray 
+validateLabels = (np.array([vectorized_result(label) for label in validateOriginalLabels])).reshape(VALIDATION_SAMPLE_SIZE,10)
 
 saveToJpeg(11,trainImages,trainLabels);
 saveToJpeg(110,trainImages,trainLabels);
@@ -54,12 +71,14 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 #train
 for i in range(BATCH_NUMBER):
-    batch_xs = trainImages[i:((i+1)*SAMPLE_SIZE_PER_BATCH-1),:]
-    batch_ys = trainLabels[i:((i+1)*SAMPLE_SIZE_PER_BATCH-1)]
+    if i%10 == 0:
+        print "Run Epoch {0}".format(i)
+    batch_xs = trainImages[i:(i+1)*SAMPLE_SIZE_PER_BATCH,:]
+    batch_ys = trainLabels[i:(i+1)*SAMPLE_SIZE_PER_BATCH,:]
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
     if i%10 == 0:
-        print("tainning accuracy: ",
-                sess.run(accuracy, feed_dict={x: validateImages, y_: validateLabels}))
+        print "Epoch {0}: accuracy {1}".format(
+                    i, sess.run(accuracy, feed_dict={x: validateImages, y_: validateLabels}))
 
 # Test trained model
 
