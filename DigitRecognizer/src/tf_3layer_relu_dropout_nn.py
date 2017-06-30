d7e909eb0281800a31980fbdfa08dcea6e575341
Zhang Shuo <shuo.1.zhang@nokia.com>
2017-06-30 13:23:38 +0800

    3layer NN with reLU and dropout, archive maximum 96% accuracy

---
+++ DigitRecognizer/src/tf_3layer_relu_dropout_nn.py
@@ -0,0 +1,60 @@
+import numpy as np
+import tensorflow as tf 
+import dataLoader as dl
+
+
+
+def init_weights(shape):
+    return tf.Variable(tf.random_normal(shape, stddev = 0.01))
+
+def model(X, w1, w2, w3, inputLayerDropoutProbability, hiddenLayerDropoutProbability):
+    X = tf.nn.dropout(X, inputLayerDropoutProbability)
+    y1 = tf.nn.relu(tf.matmul(X, w1))
+
+    y1 = tf.nn.dropout(y1, hiddenLayerDropoutProbability)
+    y2 = tf.nn.relu(tf.matmul(y1, w2))
+
+    y2 = tf.nn.dropout(y2, hiddenLayerDropoutProbability)
+
+    return tf.matmul(y2, w3)
+
+
+
+(trX,trY,teX,teY) = dl.dataLoader()
+(trainImages,trainLabels,validateImages,validateLabels) = dl.dataLoader()
+
+X = tf.placeholder("float", [None, 784])
+Y = tf.placeholder("float", [None, 10])
+
+
+w1 = init_weights([784, 625])
+w2 = init_weights([625, 625])
+w3 = init_weights([625, 10])
+"""
+w1 = tf.Variable(tf.zeros([784, 625]))
+w2 = tf.Variable(tf.zeros([625, 625]))
+w3 = tf.Variable(tf.zeros([625, 10]))
+"""
+
+# dropout parameter
+inputLayerDropoutProbability = tf.placeholder("float")
+hiddenLayerDropoutProbability = tf.placeholder("float")
+
+
+py_x = model(X, w1, w2, w3, inputLayerDropoutProbability, hiddenLayerDropoutProbability)
+
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=py_x))
+train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
+predict_op = tf.argmax(py_x, 1)
+
+with tf.Session() as sess:
+
+    tf.global_variables_initializer().run()
+
+    for i in xrange(100):
+        for start, end in zip(range(0, len(trainImages), 128), range(128, len(trainImages), 128)):
+            sess.run(train_op, feed_dict = {X: trainImages[start:end], Y: trainLabels[start:end],
+                                            inputLayerDropoutProbability: 0.8, hiddenLayerDropoutProbability: 0.5})
+        print i, np.mean(np.argmax(validateLabels, axis = 1) == sess.run(predict_op, 
+                        feed_dict = {X: validateImages, Y: validateLabels, inputLayerDropoutProbability: 1.0, hiddenLayerDropoutProbability: 1.0}))
